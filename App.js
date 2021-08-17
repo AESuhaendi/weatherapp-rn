5462e5f0f44c4c0b2c500a806a6665be92f38d8c
AE Suhaendi <adeersasuhaendi@gmail.com>
2021-08-17 22:32:02 +0700

    Weather app done

--- App.js
+++ App.js
@@ -1,30 +1,110 @@
-import React, {useState} from 'react';
+import React, {useEffect, useMemo, useReducer} from 'react';
 import {
+  ActivityIndicator,
   ImageBackground,
   KeyboardAvoidingView,
   Platform,
+  StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 import SearchInput from './src/components/SearchInput';
 import getImageForWeather from './src/utils/getImageForWeather';
+import {fetchLocationId, fetchWeather} from './src/utils/api';
+
+/** @namespace */
+const AppState = {
+  location: '',
+  loading: false,
+  error: false,
+  temperature: 0,
+  weather: '',
+};
+const reducer = (state, newState) => ({...state, ...newState});
 
 const App = () => {
-  const [location, setLocation] = useState('San Fransisco');
+  /** @type {[AppState, function(AppState): void]} */
+  const [state, setState] = useReducer(reducer, AppState);
+
+  const handleUpdateLocation = async city => {
+    if (!city) {
+      return;
+    }
+    try {
+      setState({
+        loading: true,
+      });
+
+      const locationId = await fetchLocationId(city);
+      const {location, weather, temperature} = await fetchWeather(locationId);
+
+      setState({
+        loading: false,
+        error: false,
+        location,
+        weather,
+        temperature,
+      });
+    } catch (e) {
+      setState({
+        loading: false,
+        error: true,
+      });
+    }
+  };
+
+  const imageForWeather = useMemo(
+    () => getImageForWeather(state.weather),
+    [state.weather],
+  );
+
+  useEffect(() => {
+    handleUpdateLocation('Jakarta');
+  }, []);
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <StatusBar barStyle="light-content" />
       <ImageBackground
-        source={getImageForWeather('Clear')}
+        source={imageForWeather}
         style={styles.imageContainer}
         imageStyle={styles.image}>
         <View style={styles.detailsContainer}>
-          <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
-          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-          <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+          <ActivityIndicator
+            animating={state.loading}
+            color="white"
+            size="large"
+          />
 
-          <SearchInput placeholder="Search any city" />
+          {!state.loading && (
+            <View>
+              {state.error && (
+                <Text style={[styles.smallText, styles.textStyle]}>
+                  Could not load weather, please try a different city.
+                </Text>
+              )}
+
+              {!state.error && (
+                <View>
+                  <Text style={[styles.largeText, styles.textStyle]}>
+                    {state.location}
+                  </Text>
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    {state.weather}
+                  </Text>
+                  <Text style={[styles.largeText, styles.textStyle]}>
+                    {`${Math.round(state.temperature)}°`}
+                  </Text>
+                </View>
+              )}
+
+              <SearchInput
+                placeholder="Search any city"
+                onSubmit={handleUpdateLocation}
+              />
+            </View>
+          )}
         </View>
       </ImageBackground>
     </KeyboardAvoidingView>
